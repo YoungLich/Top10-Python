e0806b34cf049db1f0610df94b78c7fd123a5799
Gabriel Proença <eduardogamesbr890@gmail.com>
2024-10-11 17:16:21 -0300

    Delete OneDrive/Área de Trabalho/PROJETOS PYTHON/python projects directory

--- OneDrive/Área de Trabalho/PROJETOS PYTHON/python projects/9 - Bot de Twitter com Tweepy.py
+++
@@ -1,9 +0,0 @@
-import tweepy
-
-auth = tweepy.OAuthHandler('CONSUMER_KEY', 'CONSUMER_SECRET')
-auth.set_access_token('ACCESS_TOKEN', 'ACCESS_TOKEN_SECRET')
-
-api = tweepy.API(auth)
-
-mensagem = "Tweet automático via Tweepy"
-api.update_status(mensagem)
